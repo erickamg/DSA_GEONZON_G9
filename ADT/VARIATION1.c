1e51ba4a438a0241443d3b98569c97414c460c65
erickamg <24101326@usc.edu.ph>
2025-09-04 13:08:49 +0800

    09-04-2025

--- ADT/VARIATION1.c
+++ ADT/VARIATION1.c
@@ -12,21 +12,32 @@ typedef struct {
 List initialize(List L);
 List insertPos(List L, int data, int position);
 List deletePos(List L, int position);
+int locate (List L, int data);
 
 int main(){
     List L;
     L = initialize(L);
-    int num = 1;
+    int num = 1, j;
 
     for(int i = 0; i < 8; i++){
         L = insertPos(L, num, i);
         num++;
     }
 
-    for(int i = 0; i < 5; i++){
+    for(int i = 0; i < 8; i++){
         printf("%d ", L.elem[i]);
     }
 
+    L = deletePos(L, 4);
+
+    printf("\n");
+
+    for(int i = 0; i < L.count; i++){
+        printf("%d ", L.elem[i]);
+    }
+
+    j = locate (L, 6);
+    printf("\nData 6 is located at position %d", j);
 
     return 0;
 }
@@ -54,5 +65,38 @@ List insertPos(List L, int data, int position){
 }
 
 List deletePos(List L, int position){
-    
+
+    int i;
+
+    if(position <= L.count){
+        for( i = 0; i < position - 1; i++){
+            
+        }
+
+        for(int x = i; x < L.count; x++){
+            L.elem[x] = L.elem[x + 1];
+        }
+
+        L.count--;
+    }
+
+    return L;
 }
+
+int locate (List L, int data){
+
+    int i, x;
+
+    for(i = 0; i <=L.count ; i++){
+        if(L.elem[i] == data){
+            return i + 1;
+            x = 1;
+        }
+    }
+
+    if(x != 1){
+        return -1;
+    }
+
+}
+
