d4adb2579f8b31dbdc0eb97f5901676a5665a5c9
Ericka <24101326@usc.edu.ph>
2025-09-11 23:51:56 +0800

    09-11-2025

--- ADT/VARIATION2.c
+++ ADT/VARIATION2.c
@@ -12,6 +12,9 @@ typedef struct {
 void initialize(EPtr L);
 void insertPos(EPtr L, int data, int position);
 void deletePos(EPtr L, int position);
+int locate(EPtr L, int data);
+int retrieve(EPtr L, int position);
+void insertSorted(EPtr L, int data);
 
 int main(){
     EPtr L;
@@ -27,7 +30,7 @@ void insertPos(EPtr L, int data, int position){
 
     if(L->count < MAX){
         if(position >= 0 && position <= L->count){
-            for(int i = L->count - 1; i != position - 1; i++){
+            for(int i = L->count - 1; i != position - 1; i--){
                 L->elem[i + 1] = L->elem[i];
             }
 
@@ -39,10 +42,60 @@ void insertPos(EPtr L, int data, int position){
 }
 
 void deletePos(EPtr L, int position){
+    int i;
 
     if(position <= L->count){
+        if(position >= 0 && position <= L->count){
+            for(i = 0; i != position - 1; i++){
+                
+            }
 
+            for(int x = i; x < L->count; x++){
+                L->elem[x] = L->elem[x + 1];
+            }
+
+            L->count--;
+
+        }
     }
-    
+
 }
 
+int locate(EPtr L, int data){
+
+    int i;
+
+    for(i = 0; L->elem[i] != data && i < L->count; i++){
+        
+    }
+
+    if(L->elem[i] == data){
+        return i + 1;
+    }else{
+        return -1;
+    }
+
+}
+
+int retrieve(EPtr L, int position){
+
+    if(position <= L->count && position >= 0){
+        for(int i = 0; i + 1 != position; i++ ){
+
+        }
+
+        if(i + 1 == position){
+            return L->elem[i];
+        }else{
+            return -1;
+        }
+    }
+}
+
+void insertSorted(EPtr L, int data){
+
+    if(){
+        
+    }
+
+}
