da4bad5b6cd7157c512c36abbfd1a48c16dba355
Ericka <24101326@usc.edu.ph>
2025-09-12 01:19:50 +0800

    9-12

--- ADT/VARIATION3.c
+++ ADT/VARIATION3.c
@@ -1,1 +1,17 @@
 /* DYNAMIC ARRAY AND ACCESSED BY VALUE*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#define LENGTH 10
+
+typedef struct{
+    int *elemPtr;
+    int count;
+    int max;
+}List;
+
+
+
+int main(){
+
+}
