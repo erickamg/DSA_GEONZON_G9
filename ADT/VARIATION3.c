6b43f74c941e90901f0181eaf6e87654432778f6
Ericka <24101326@usc.edu.ph>
2025-09-13 12:09:40 +0800

    VAR3_FINAL

--- ADT/VARIATION3.c
+++ ADT/VARIATION3.c
@@ -10,127 +10,6 @@ typedef struct{
     int max;
 }List;
 
-List intialize(List L);
-List insertPos(List L, int data, int position);
-List deletePos(List L, int position);
-int locate(List L, int data);
-List insertSorted(List L, int data);
-void display(List L);
-List resize(List L);
-
-int main(){
-    List L;
-}
-
-List intialize(List L){
-    L.elemPtr = (int*)malloc(LENGTH * sizeof(int));
-    L.max = LENGTH;
-    L.count = 0;
-
-    return L;
-}
-
-List insertPos(List L, int data, int position){
-    int i;
-    
-    if(L.count < L.max){
-        if(position - 1 <= L.count && position >= 0){
-            for(i = 0; i != position - 1; i++){
-                L.elemPtr[i + 1] = L.elemPtr[i];
-            }
-            
-            L.elemPtr[i] = data;
-            L.count++;
-        }
-    }else{
-        resize(L);
-        L = insertPos(L, data, position);
-    }
-
-    return L;
-}
-
-List deletePos(List L, int position){
-    
-    int i, x;
-    
-    if(position <= L.count){
-        for(i = 0; i != position - 1; i++){
-            
-        }
-        
-        for(x = i; x <= L.count - 1; x++){
-            L.elemPtr[x] = L.elemPtr[x + 1];
-        }
-        
-        L.count--;
-    }
-    
-    return L;
-}
-
-int locate(List L, int data){
-    
-    int i;
-    
-    for(i = 0; L.elemPtr[i] != data; i++){
-        
-    }
-    
-    if(L.elemPtr[i] == data){
-        return i + 1;
-    }else{
-        return -1;
-    }
-}
-
-List insertSorted(List L, int data){
-    
-    if(L.count < L.max){
-       
-       for(int i = L.count - 1; data < L.elemPtr[i]; i--){
-           L.elemPtr[i + 1] = L.elemPtr[i];
-       }
-       
-       L.elemPtr[i] = data;
-       L.count++;
-       
-    }else{
-        resize(L);
-        L = insertSorted(L, data);
-    }
-    
-    return L;
-}
-
-void display(List L){
-    
-    for(int i = 0; i <L.count; i++){
-        printf("%d ", L.elemPtr[i]);
-    }
-    
-}
-
-List resize(List L){
-
-    L.elemPtr = (int*)realloc(L.elemPtr, (LENGTH * 2)*sizeof(int));
-    L.max = LENGTH * 2;
-
-    return L;
-}
-
-/*
-
-#include <stdio.h>
-#include <stdlib.h>
-#define LENGTH 10
-
-typedef struct{
-    int *elemPtr;
-    int count;
-    int max;
-}List;
-
 List initialize(List L);
 List insertPos(List L, int data, int position);
 List deletePos(List L, int position);
@@ -142,22 +21,28 @@ List resize(List L);
 int main(){
     List L = initialize(L);
     
+    printf("Array after inserting at position:");
     for(int i = 0; i < 8; i++){
-        L = insertPos(L, i + 1, i + 1);
+        L = insertPos(L, i + 1, i);
     }
     
     display(L);
     
+    printf("Array after deleting at position: ");
     L = deletePos(L, 5);
     display(L);
     
     int loc;
-    printf("\nEnter location of data: ");
+    printf("Enter location of data: ");
     scanf("%d", &loc);
-    int l = locate(L, loc);
-    printf("\nData located at position %d: %d\n", loc, l);
+    if(loc <= L.count){
+        int l = locate(L, loc);
+        printf("\nData located at position %d: %d\n", loc, l);
+    }else{
+        printf("Data cannot be located.");
+    }
     
-    
+    printf("\nArray after inserting sorted: ");
     L = insertSorted(L, 4);
     display(L);
     return 0;
@@ -229,6 +114,10 @@ int locate(List L, int data){
 List insertSorted(List L, int data){
     int i;
     
+    if(L.count >= L.max){
+        L = resize(L);
+    }
+    
     if(L.count < L.max){
        
        for(i = L.count - 1; data < L.elemPtr[i]; i--){
@@ -238,9 +127,6 @@ List insertSorted(List L, int data){
        L.elemPtr[i] = data;
        L.count++;
        
-    }else{
-        resize(L);
-        L = insertSorted(L, data);
     }
     
     return L;
@@ -248,11 +134,10 @@ List insertSorted(List L, int data){
 
 void display(List L){
     
-    printf("\n");
-    
     for(int i = 0; i <L.count; i++){
         printf("%d ", L.elemPtr[i]);
     }
+    printf("\n");
     
 }
 
@@ -263,5 +148,3 @@ List resize(List L){
 
     return L;
 }
-
-*/
